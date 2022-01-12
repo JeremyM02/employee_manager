012116f4e22cd61c2378aaeb695840f7873916fe
JeremyM02 <zom7@fresnou.org>
2022-01-12 08:54:16 -0800

    Edit feature for employees may be finished, but not tested.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,6 @@ const employeeController = require('../controllers/employeeController');
 router.get('/', employeeController.displayEmployees);
 router.get('/add', employeeController.renderAddEmployeeForm);
 router.post('/add', employeeController.addEmployee);
+router.get('/update/:id', employeeController.renderUpdateForm);
+router.post('/update/:id', employeeController.updateEmployee);
 module.exports = router;
